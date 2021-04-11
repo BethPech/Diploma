f4ffd979be1882471eb7262023f57406c702ff90
BethPech <Colnze17-94@mail.ru>
2021-04-11 11:40:57 +0300

    Smth changed

---
+++ src/test/java/ru/netology/page/MainPage.java
@@ -0,0 +1,20 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class MainPage {
+    private static final SelenideElement buyWithDebitCardButton = $(byText("Купить"));
+    private static final SelenideElement buyWithCreditCardButton = $(byText("Купить в кредит"));
+
+    public void payWithDebitCard(){
+        buyWithDebitCardButton.click();
+
+
+    }
+    public void payWithCreditCard(){
+        buyWithCreditCardButton.click();
+    }
+}
